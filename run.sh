bebe79ba137b335d5dcd54334094fa4b806d6cc1
Gops <154628241+nonoto999t@users.noreply.github.com>
2024-01-27 12:25:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+
 while true; do
-    python FULLCLICK3.py  # รันโปรแกรม Python
-    EXIT_CODE=$?       # เก็บรหัสออกของโปรแกรม Python
-    sleep 20            # หยุดการทำงานของ script นี้เป็นเวลา 5 วินาที
+    python /path/to/FULLCLICK3.py  # รันโปรแกรม Python
+    EXIT_CODE=$?                   # เก็บรหัสออกของโปรแกรม Python
+    sleep 20                        # หยุดการทำงานของ script นี้เป็นเวลา 20 วินาที
 
     # ตรวจสอบสถานะการทำงานของโปรแกรม Python
     if [ $EXIT_CODE -eq 0 ]; then
         echo "โปรแกรมทำงานเสร็จสมบูรณ์"
+        break  # ออกจาก loop หากโปรแกรมทำงานเสร็จสิ้น
     else
         echo "มีข้อผิดพลาดในการทำงาน รหัส: $EXIT_CODE"
     fi
